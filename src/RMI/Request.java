78710fc96b439153b3d438fa84a6802608868b64
brandoncc94 <brandoncc94@gmail.com>
2015-11-11 14:13:16 -0600

    Inicio

---
+++ src/RMI/Request.java
@@ -0,0 +1,28 @@
+
+package RMI;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Request {
+    Registry registry;
+    IFunctions service;
+
+    public Registry getRegistry() {
+        return registry;
+    }
+
+    public IFunctions getService() {
+        return service;
+    }
+
+    public void setRegistry(String pIp, int pPort) throws RemoteException {
+        this.registry = LocateRegistry.getRegistry(pIp, pPort);
+    }
+
+    public void setService(String pService) throws RemoteException, NotBoundException {
+        this.service = (IFunctions) registry.lookup(pService);
+    }
+}
---
+++ src/clientermi/ClientRMI.java
@@ -0,0 +1,71 @@
+
+package clientermi;
+
+import RMI.Request;
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+public class ClientRMI {
+
+    private void showMenu(){
+        while(true){
+            System.out.println("****************************");
+            System.out.println("\t    Menú");
+            System.out.println("1.CREATE.");
+            System.out.println("2.MKDIR.");
+            System.out.println("3.FILE.");
+            System.out.println("4.CD.");
+            System.out.println("5.LS.");
+            System.out.println("6.DU.");
+            System.out.println("7.PWD.");
+            System.out.println("8.CAT.");
+            System.out.println("9.CPY.");
+            System.out.println("10.MV.");
+            System.out.println("11.RM.");
+            System.out.println("12.FIND.");
+            System.out.println("13.TREE.");
+            System.out.println("14.EXIT.");
+            System.out.println("****************************\n");
+            
+            System.out.print("Digite una opción: ");
+            Scanner scanIn = new Scanner(System.in);
+            String opc = scanIn.nextLine();
+            
+            switch(opc){
+                case "1":
+                    try{
+                        Request request = new Request();
+                        request.setRegistry("127.0.0.1", 1099);
+                        request.setService("CREATE");
+                        System.out.print("Digite el tamaño del disco virtual: ");
+                        int size = Integer.parseInt(scanIn.nextLine());
+                        request.getService().create(size);
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "2":
+                    try{
+                        Request request = new Request();
+                        request.setRegistry("127.0.0.1", 1099);
+                        request.setService("MKDIR");
+                        System.out.println("Digite el nombre del directorio: ");
+                        String dirName = scanIn.nextLine();
+                        request.getService().MKDIR(dirName);
+                    }catch(RemoteException | NotBoundException | NumberFormatException e){
+                        System.out.println("Error: " + e.getLocalizedMessage());
+                    }
+                    break;
+                case "14":
+                    System.exit(0);
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        ClientRMI client = new ClientRMI();
+        client.showMenu();
+    } 
+}
