89f293b98917c824e347f46730852c35065a3159
Juan Carlos Martinez <juancar199400@gmail.com>
2015-11-12 10:50:58 -0600

    Agregado ls, file y mv en progreso.

--- src/clientermi/ClientRMI.java
+++ src/clientermi/ClientRMI.java
@@ -18,7 +18,9 @@ public class ClientRMI {
         while(true){
             String path = request.getService().getPath(root);
             System.out.print(path+">");
-            String[] params = scanIn.nextLine().split(" ");
+            String message = scanIn.nextLine();
+            //Quita espacios antes de la entrada y espacios entre palabra se reducen a 1
+            String[] params = message.trim().replaceAll("( )+", " ").split(" ");
             params[0] = params[0].toLowerCase();
             switch(params[0]){
                 case "cd":
@@ -37,13 +39,49 @@ public class ClientRMI {
                         System.out.println(e.getMessage());
                     }
                     break;
+                case "ls":
+                    try{
+                        System.out.print(request.getService().ls(root));
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "pwd":
+                    try{
+                        System.out.println("El directorio actual es: "+request.getService().getPath(root));
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "mv": //MOVE FILE OR DIRECTORY
+                    try{
+                        if(!(params.length > 1)){
+                            System.out.println("Faltan parametros en comando");
+                            break;
+                        }else{
+                            boolean moved = request.getService().mv(params,root);
+                            if(moved){
+                                System.out.println("Directorio creado exitosamente.");
+                            }else{
+                                System.out.println("No se puede crear directorio con este nombre."); 
+                            }
+                        }
+                    }catch(RemoteException | NumberFormatException e){
+                        System.out.println("Error: " + e.getLocalizedMessage());
+                    }
+                    break;
                 case "mkdir":
                     try{
                         if(!(params.length > 1)){
                             System.out.println("Faltan parametros en comando");
                             break;
                         }else{
-                            request.getService().mkdir(params[1],root);
+                            boolean created = request.getService().mkdir(params[1],root);
+                            if(created){
+                                System.out.println("Directorio creado exitosamente.");
+                            }else{
+                                System.out.println("No se puede crear directorio con este nombre."); 
+                            }
                         }
                     }catch(RemoteException | NumberFormatException e){
                         System.out.println("Error: " + e.getLocalizedMessage());
